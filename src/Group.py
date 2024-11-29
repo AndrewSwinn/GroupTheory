76eb1ecd96637e4d267ae0b2f07897a5bbc24aed
Andrew <azswinn@gmail.com>
2024-11-29 09:51:29 +0000

    Element attributes converted to arrays to allow group multiplication

--- src/Group.py
+++ src/Group.py
@@ -1,11 +1,12 @@
 from collections import deque
+import numpy as np
 
 
 class Element:
-    def __init__(self, name, number, permutation):
+    def __init__(self, name, number, permutations):
         self.name = name
         self.number = number
-        self.permutation = permutation
+        self.permutations = permutations
 
     def __str__(self):
 
@@ -17,9 +18,14 @@ class Group:
     def __init__(self, **kwargs):
 
         generators = kwargs.get('generators')
+        factors    = kwargs.get('factors')
 
         if generators is not None:
             self.elements = self._generate_group(generators)
+        elif factors is not None:
+            self.elements = self._direct_product(factors)
+
+
 
     def _pretty_name(self, name):
         pretty_name = ''
@@ -36,6 +42,17 @@ class Group:
 
         return pretty_name
 
+    def _direct_product(self,factors):
+
+        base = factors[0]
+
+        for factor in factors[1:]:
+            for factor_element in factor.elements:
+                for element in base.elements:
+                    pass
+
+
+
     def _generate_group(self, generators):
         #create identity
         permute_length = 1
@@ -43,7 +60,7 @@ class Group:
             for cycle in generator:
                 permute_length = max(permute_length, max(cycle))
 
-        identity = Element(name=['e'], number=1, permutation=[tuple([i for i in range(1, permute_length + 1)])])
+        identity = Element(name=['e'], number=[1], permutations=[tuple([i for i in range(1, permute_length + 1)])])
 
 
         #create group (unsorted and unnumbered)
@@ -52,7 +69,7 @@ class Group:
         while new:
             new, new_members = False, []
             for member in group:
-                member_name, member_permutation = member.name[0], member.permutation[0]
+                member_name, member_permutation = member.name[0], member.permutations[0]
                 for name, cycles in generators.items():
                     orig_permutation = list(member_permutation)
                     work_permutation = list(member_permutation)
@@ -63,9 +80,9 @@ class Group:
                         for i, element in enumerate(sublist):
                             work_permutation[cycle[i] - 1] = orig_permutation[sublist[i] - 1]
 
-                    if (tuple(work_permutation) not in [element.permutation[0] for element in group]) and (
-                            tuple(work_permutation) not in [element.permutation for element in new_members]):
-                        new_members.append(Element([new_name], 0,  [tuple(work_permutation)]))
+                    if (tuple(work_permutation) not in [element.permutations[0] for element in group]) and (
+                            tuple(work_permutation) not in [element.permutations for element in new_members]):
+                        new_members.append(Element([new_name], [0],  [tuple(work_permutation)]))
 
             if len(new_members) > 0:
                 new = True
@@ -75,8 +92,8 @@ class Group:
         next = 2
         group.sort(key=lambda x: x.name)
         for element in group:
-            if element.number != 1:
-                element.number = next
+            if element.number != [1]:
+                element.number = [next]
                 next += 1
         group.sort(key=lambda x: x.number)
 
@@ -84,11 +101,15 @@ class Group:
 
     # Takes 2 elements of the group, multiplies them together and returns the product
     def element_multiply(self, element1, element2):
-        perm1, perm2 = list(element1.permutation), list(element2.permutation)
-        result = tuple([perm1[i - 1] for i in perm2])
+        permutations1, permutations2 = element1.permutations, element2.permutations
+        result = []
+        for i, perm1 in enumerate(permutations1):
+            perm2 = permutations2[i]
+            result.append(tuple([perm1[i - 1] for i in perm2]))
         for element in self.elements:
-            if element.permutation == result:
-                return element
+            for i, permutation in enumerate(element.permutations):
+                if permutation == result[i]:
+                    return element
 
 
 
@@ -96,11 +117,26 @@ class Group:
 
 if __name__ == "__main__":
 
-    g = Group(generators={'r': [(1,2,3,4,5,6)], 'f':[(2,6),(3,5)]})
+    C3 = Group(generators={'r': [(1,2,3)]})
+    C4 = Group(generators={'r': [(1,2,3,4)]})
 
-    for e in g.elements:
-        print(e, e.number, e.permutation)
+    C3C4 = Group(factors=[C3, C4])
 
 
 
-    #print(g.element_multiply('f', 'f'))
+    def multiplication_table(group):
+        order = len(group.elements)
+        table = np.zeros(shape=(order, order))
+
+        for i in group.elements:
+            for j in group.elements:
+                product = group.element_multiply(i, j)
+                table[i.number[0] - 1, j.number[0] - 1] = product.number[0]
+        return table
+
+
+    mult = multiplication_table(C4)
+
+
+
+    print(mult)
